5bdfa3b2cdb9fd80df1c29d58cb56828fb7a6cce
Andrey Khudoyarov <andrei.hudojaroff@yandex.ru>
2022-06-25 12:51:20 +0400

    Changes in 4, 5, 8, 9 requests

--- Advanced_Select.sql
+++ Advanced_Select.sql
@@ -14,20 +14,18 @@ join albums a on t.albumid = a.albumid
 group by title;
 
 -- все исполнители, которые не выпустили альбомы в 2020 году;
-select singer_name from singers s  
+select distinct singer_name from singers s  
 join singersalbums s3 on s.singerid = s3.singerid
 join albums a on s3.albumid = a.albumid
-where year != 2020
-group by singer_name;
+where year != 2020;
 
 -- названия сборников, в которых присутствует конкретный исполнитель;
-select col_name from collections c 
+select distinct col_name from collections c 
 join collectionstracks c2 on c.collectionid = c2.collectionid
 join tracks t on c2.trackid = t.trackid
 join singersalbums s on t.albumid = s.albumid
 join singers s2 on s.singerid = s2.singerid
-where s2.singerid = 4
-group by col_name;
+where s2.singerid = 4;
 
 -- название альбомов, в которых присутствуют исполнители более 1 жанра;
 select title from albums a
@@ -44,16 +42,15 @@ left join collectionstracks c on t.trackid = c.trackid
 where collectionid is null;
 
 -- исполнителя(-ей), написавшего самый короткий по продолжительности трек (теоретически таких треков может быть несколько);
-select singer_name from singers s
+select distinct singer_name from singers s
 join singersalbums s2 on s.singerid = s2.singerid
 join albums a on s2.albumid = a.albumid
 join tracks t on t.albumid = a.albumid
-where t.duration = (select min(duration) from tracks)
-group by singer_name;
+where t.duration = (select min(duration) from tracks);
 
 
 -- название альбомов, содержащих наименьшее количество треков.
-select title, count(trackid) from albums a
+select title, count(*) from albums a
 left join tracks t on t.albumid = a.albumid
 group by title
-having count(*) = 1;
+having count(*) = (select count from (select title, count(*) from albums a left join tracks t on t.albumid = a.albumid  group by title order by count(*) limit 1) as num);
